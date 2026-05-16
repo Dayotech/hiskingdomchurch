16702d676c14f555b50a09f96b77772ac9c72fa3
semiu olaniyan <semiuolaniyan4@gmail.com>
2026-05-16 05:22:04 +0100

    additional to css and js

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
-const fadeElements = document.querySelectorAll('.fade-up');
+ const navMenu = document.getElementById("navMenu");
+const menuBtn = document.getElementById("menuBtn");
+const topBtn = document.querySelector(".top-btn");
 
-window.addEventListener('scroll', () => {
-  fadeElements.forEach(el => {
-    const top = el.getBoundingClientRect().top;
+if (menuBtn && navMenu) {
+  menuBtn.addEventListener("click", function () {
+    navMenu.classList.toggle("show");
 
-    if(top < window.innerHeight - 100){
-      el.classList.add('show');
+    if (navMenu.classList.contains("show")) {
+      menuBtn.innerHTML = "✖";
+    } else {
+      menuBtn.innerHTML = "☰";
+    }
+  });
+}
+
+const navLinks = document.querySelectorAll(".nav a");
+
+navLinks.forEach(function (link) {
+  link.addEventListener("click", function () {
+    navMenu.classList.remove("show");
+
+    if (menuBtn) {
+      menuBtn.innerHTML = "☰";
     }
   });
 });
 
-function toggleDarkMode() {
-  document.body.classList.toggle('dark-mode');
+window.addEventListener("scroll", function () {
+  if (topBtn) {
+    if (window.scrollY > 300) {
+      topBtn.style.display = "block";
+    } else {
+      topBtn.style.display = "none";
+    }
+  }
+});
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
 }
 
-window.addEventListener('load', function(){
-  document.getElementById('loader').style.display = 'none';
-});
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
